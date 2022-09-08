c318e440887361ee1bec86bbb90b4c7c1b402792
Jonatas Luiz de Oliveira <contact@jonatasoliveira.dev>
2022-09-08 09:10:20 -0300

    add food and change grid from transform to position

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, time::FixedTimestep};
 mod snake;
-use snake::spawn_snake;
+use snake::{movement_system, spawn_system};
+pub mod components;
+pub mod food;
+pub mod grid;
 
 fn main() {
     App::new()
+        .insert_resource(WindowDescriptor {
+            title: "Snake Game".to_string(),
+            width: 500.0,
+            height: 500.0,
+            ..default()
+        })
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
         .add_startup_system(setup_camera)
-        .add_startup_system(spawn_snake)
+        .add_startup_system(spawn_system)
         .add_plugins(DefaultPlugins)
+        .add_system(movement_system)
+        .add_system_set(
+            SystemSet::new()
+                .with_run_criteria(FixedTimestep::step(1.0))
+                .with_system(food::spawn_system),
+        )
+        .add_system_set_to_stage(
+            CoreStage::PostUpdate,
+            SystemSet::new()
+                .with_system(grid::position_translation)
+                .with_system(grid::size_scaling),
+        )
         .run();
 }
 
--- src/snake.rs
+++ src/snake.rs
@@ -1,11 +1,12 @@
+use crate::components::{Position, Size};
 use bevy::prelude::*;
 
 const SNAKE_HEAD_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
 
 #[derive(Component)]
-pub struct SnakeHead;
+pub struct Head;
 
-pub fn spawn_snake(mut commands: Commands) {
+pub fn spawn_system(mut commands: Commands) {
     commands
         .spawn_bundle(SpriteBundle {
             sprite: Sprite {
@@ -18,7 +19,30 @@ pub fn spawn_snake(mut commands: Commands) {
             },
             ..default()
         })
-        .insert(SnakeHead);
+        .insert(Head)
+        .insert(Position { x: 3, y: 3 })
+        .insert(Size::square(0.8));
+}
+
+#[allow(clippy::needless_pass_by_value)]
+pub fn movement_system(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut head_positions: Query<&mut Position, With<Head>>,
+) {
+    for mut position in head_positions.iter_mut() {
+        if keyboard_input.pressed(KeyCode::D) {
+            position.x += 1;
+        }
+        if keyboard_input.pressed(KeyCode::W) {
+            position.y += 1;
+        }
+        if keyboard_input.pressed(KeyCode::A) {
+            position.x -= 1;
+        }
+        if keyboard_input.pressed(KeyCode::S) {
+            position.y -= 1;
+        }
+    }
 }
 
 #[cfg(test)]
@@ -30,16 +54,102 @@ mod test {
         // 1 Inicialização do App
         let mut app = App::new();
 
-        // 2 Adicionar o `spawn_snake` startup system
-        app.add_startup_system(spawn_snake);
+        // 2 Adicionar o `spawn_system` startup system
+        app.add_startup_system(spawn_system);
 
         // 3 Executar todos os sistemas pelo menos uma vez
         app.update();
 
-        // 4 Fazer uma query por entidades que contenham o componente `SnakeHead`
-        let mut query = app.world.query_filtered::<Entity, With<SnakeHead>>();
+        // 4 Fazer uma query por entidades que contenham o componente `Head`
+        let mut query = app.world.query_filtered::<Entity, With<Head>>();
 
         // 5 Verificar se a contagem de componentes da query foi igual a 1
         assert_eq!(query.iter(&app.world).count(), 1);
+    }
+    #[test]
+    fn snake_head_has_moved_up() {
+        // Setup
+        let mut app = App::new();
+        let default_position = Position { x: 3, y: 4 };
+
+        // Adicionando sistemas
+        app.add_startup_system(spawn_system)
+            .add_system(movement_system);
+
+        // Adicionando inputs de `KeyCode`s
+        let mut input = Input::<KeyCode>::default();
+        input.press(KeyCode::W);
+        app.insert_resource(input);
+
+        // Executando sistemas pelo menos uma vez
+        app.update();
+
+        // Query para obter entidades com `Head` e `Position`
+        let mut query = app.world.query::<(&Head, &Position)>();
+
+        // Verificando se o valor de Y no `Position` mudou
+        query.iter(&app.world).for_each(|(_head, position)| {
+            assert_eq!(&default_position, position);
+        })
+    }
+    #[test]
+    fn snake_head_moves_up_and_right() {
+        // Setup
+        let mut app = App::new();
+        let up_position = Position { x: 3, y: 4 };
+
+        // Adiciona systemas
+        app.add_startup_system(spawn_system)
+            .add_system(movement_system);
+
+        // Testa movimento para cima
+        let mut input = Input::<KeyCode>::default();
+        input.press(KeyCode::W);
+        app.insert_resource(input);
+        app.update();
+
+        let mut query = app.world.query::<(&Head, &Position)>();
+        query.iter(&app.world).for_each(|(_head, position)| {
+            assert_eq!(position, &up_position);
+        });
+
+        // Testa movimento para direita
+        let up_right_position = Position { x: 4, y: 4 };
+        let mut input = Input::<KeyCode>::default();
+        input.press(KeyCode::D);
+        app.insert_resource(input);
+        app.update();
+
+        let mut query = app.world.query::<(&Head, &Position)>();
+        query.iter(&app.world).for_each(|(_head, position)| {
+            assert_eq!(&up_right_position, position);
+        })
+    }
+    #[test]
+    fn snake_head_moves_down_and_left() {
+        // Setup
+        let mut app = App::new();
+        let down_left_position = Position { x: 2, y: 2 };
+
+        app.add_startup_system(spawn_system)
+            .add_system(movement_system);
+
+        // Movimenta para baixo
+        let mut input = Input::<KeyCode>::default();
+        input.press(KeyCode::S);
+        app.insert_resource(input);
+        app.update();
+
+        // Movimenta para esquerda
+        let mut input = Input::<KeyCode>::default();
+        input.press(KeyCode::A);
+        app.insert_resource(input);
+        app.update();
+
+        // Assert
+        let mut query = app.world.query::<(&Head, &Position)>();
+        query.iter(&app.world).for_each(|(_head, position)| {
+            assert_eq!(&down_left_position, position);
+        })
     }
 }
